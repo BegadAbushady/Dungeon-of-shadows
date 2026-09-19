3a6f0d789ab4ed85f3bcc1a4b7cfd20e4da2c830
begaad-1 <begaadmohamed7@gmail.com>
2026-09-20 02:37:41 +0300

    Update dungeon.py
    fixing for errors and adding comments all along the project

--- dungeon.py
+++ dungeon.py
@@ -7,18 +7,22 @@ player = {"name": input("Enter your name: "), "hp": 100,
             "potions": 3, "potion_capacity": 20
             , "level": 1, "base_defense" : 10}
 Goblin = {"name": "Goblin", "hp": 40, "attack": 10, "defense": 5, "base_defense": 5, "xp": 40}
-Orc = {"name": "Orc", "hp": 60, "attack": 15, "defense": 8,"base_defense": 5, "xp": 100}
+Orc = {"name": "Orc", "hp": 60, "attack": 15, "defense": 8,"base_defense": 8, "xp": 100}
 Skeleton = {"name": "Skeleton", "hp": 50, "attack": 12, "defense": 6,"base_defense": 6, "xp": 75}
 Dark_Knight = {"name": "Dark_Knight", "hp": 80, "attack": 20, "defense": 12, "base_defense": 12, "xp": 150}
 Dragon = {"name": "Dragon", "hp": 350, "attack": 40, "defense": 30, "base_defense": 30, "xp": 450}
+
+#all character's dictionaries assigned 
+
 normal_enemies = [Goblin, Orc, Skeleton, Dark_Knight]
+
+#all enemies except for the Dragon for he is the boss
 
 time.sleep(0.5)
 
 def game_start(player_stats,enemy_stats):
 
-
-      
+      #the whole start of the game
       print("\n\n===============================")
       print("      DUNGEON OF SHADOWS       ")
       print("===============================\n\n")
@@ -31,8 +35,11 @@ def game_start(player_stats,enemy_stats):
       time.sleep(1.5)
       print("You encounter a " + enemy_stats["name"] + "!") if enemy_stats != Orc else print("You encounter an " + enemy_stats["name"] + "!")
       time.sleep(1.5)
-def create_enemy(template):
+
       
+def create_enemy(template):
+      #creating a template of the chosen enemy
+      #important for not changing the standard enemy values
       return {
             "name": template["name"],
             "hp": template["hp"],
@@ -43,19 +50,26 @@ def create_enemy(template):
       }
 
 def showing_player_stats(player_stats):
+      # Displayed after every battle round so the player can see
+      # temporary defense changes and current HP.
       print(f"\n\nPlayer: {player_stats["name"]}\n")
       print(f"HP: {player_stats["hp"]}\n")
       print(f"Attack: {player_stats["attack"]}\n")
       print(f"Defense: {player_stats["defense"]}\n")
       print(f"level: {player_stats["level"]}\n")
       time.sleep(2)     
+      
 
 def showing_enemy_stats(enemy_stats):
+     #it shows the enemy stats on the screen
+     #will be called on every turn
      print(f"\n\nHP: {enemy_stats['hp']}")
      print(f"Attack: {enemy_stats['attack']}")
      print(f"Defense: {enemy_stats['defense']}\n\n")
 
 def player_turn(choice, player_stats, enemy_stats):
+      #player choices are handled here
+      
       match choice:
             case '1':
                   damage =  player_stats['attack'] - enemy_stats['defense']
@@ -96,6 +110,8 @@ def player_turn(choice, player_stats, enemy_stats):
                         print(f"You took {damage} damage!\n\n")
 
 def enemy_turn(player_stats, enemy_stats):
+      #the enemy randomly chooses to attack or defend
+      #based on random.random function
       print("enemy's turn!\n\n")
       if random.random() > 0.5:
             print("The enemy is attacking you")
@@ -113,7 +129,9 @@ def enemy_turn(player_stats, enemy_stats):
             enemy_stats['defense'] += 10
 
 def game_loop(player_stats, enemy_stats):
-      while player_stats['hp'] > 0 and enemy_stats['hp'] > 0:
+      #the core of the game's logic
+      
+      while player_stats['hp'] > 0 and enemy_stats['hp'] > 0: #the loop continues till the player or enemy's hp is 0
             print("What will you do?\n")
             print("1. Attack\n")
             print("2. Defend\n")
@@ -132,7 +150,7 @@ def game_loop(player_stats, enemy_stats):
             showing_enemy_stats(enemy_stats)
 
 def finish_game(player_stats, enemy_stats):
-
+       #the function to end the game and check if the player still wants to continue and weither he wants to face the boss or not
        if player_stats['hp'] <= 0:
              print("Game Over! You have been defeated!\n\n")
        else:
@@ -163,9 +181,10 @@ def finish_game(player_stats, enemy_stats):
             print("game is closing...")
             time.sleep(2)
             return None
-       exit()
-                        
+
+                                    
 def leveling_up(player_stats,xp):
+      #this function controls leveling up logic
       print(f"Congratulations! You have won {xp} XP!")
       player_stats['xp'] += xp
       new_level = player_stats['xp'] // 100 + 1
@@ -180,7 +199,7 @@ def leveling_up(player_stats,xp):
             print("All your stats has increased and enhanced!!")
 
 temp = create_enemy(random.choice(normal_enemies))
-while temp!=None:
+while temp!=None: #the loop continues till the player choses to end the game
     game_start(player,temp)
     showing_enemy_stats(temp)
     game_loop(player, temp)
